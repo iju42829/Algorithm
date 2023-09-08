24b958deda00a357136b3278a1f3cb6c0d2fe08d
iju42829 <ijud4282@gmail.com>
2023-09-08 19:57:32 +0900

    solve 11508

---
+++ src/baekjoon/Problem11508.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Problem11508 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int result = 0;
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            if (i % 3 == 2)
+                continue;
+
+            result += list.get(i);
+        }
+
+        bw.write(result + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
