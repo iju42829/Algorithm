5161f774da06ce8f7c51eb844493950fa73ad7be
iju42829 <ijud4282@gmail.com>
2023-09-09 18:30:09 +0900

    refactoring

--- src/baekjoon/bronze/Problem1001.java
+++ src/baekjoon/bronze/Problem1001.java
@@ -1,14 +1,14 @@
-package baekjoon;
+package baekjoon.bronze;
 
 import java.util.Scanner;
 
-public class Problem1000 {
+public class Problem1001 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println(a + b);
+        System.out.println(a - b);
     }
 }
--- src/baekjoon/gold/Problem14719.java
+++ src/baekjoon/gold/Problem14719.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.gold;
 
 import java.io.*;
 
