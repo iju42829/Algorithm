11cbd0e545246a47d07775d0e6e082dedc5f7f5c
iju42829 <ijud4282@gmail.com>
2023-11-02 13:27:55 +0900

    solve 16953

---
+++ src/baekjoon/silver/Problem16953.java
@@ -0,0 +1,48 @@
+package baekjoon.silver;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem16953 {
+    private static long result = Long.MAX_VALUE;
+
+    public static void main(String[] args) {
+        solution();
+    }
+
+    private static void solution() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            long a = Integer.parseInt(st.nextToken());
+            long b = Integer.parseInt(st.nextToken());
+
+            dfs(a, b, 1);
+
+            if (result == Long.MAX_VALUE) {
+                bw.write("-1\n");
+
+            } else {
+                bw.write(result + "\n");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void dfs(long currentNumber, long n, long depth) {
+        if (currentNumber > n) {
+            return;
+        }
+
+        if (currentNumber == n) {
+            result = Math.min(result, depth);
+        }
+
+        dfs(currentNumber * 2, n, depth + 1);
+        dfs(Long.parseLong(currentNumber + "1"), n, depth + 1);
+    }
+}
