8fffeb649e6e017231ff500edc3c3ab95c329950
iju42829 <ijud4282@gmail.com>
2025-04-06 19:19:14 +0900

    BOJ - solve 1094 (막대기)

---
+++ src/baekjoon/silver/silver_V/Problem1094.java
@@ -0,0 +1,33 @@
+package baekjoon.silver.silver_V;
+
+import java.io.*;
+
+public class Problem1094 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int result = solve();
+        bw.write(result + "\n");
+
+        bw.close();
+        br.close();
+    }
+
+    private static int solve() throws IOException {
+        int x = Integer.parseInt(br.readLine());
+        int stick = 64;
+        int count = 0;
+
+        while (x > 0) {
+            if (stick > x) stick /= 2;
+            else {
+                x -= stick;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
