67046076df55fb95d591cb356483c0a5eb0671c6
iju42829 <ijud4282@gmail.com>
2025-05-22 13:30:14 +0900

    BOJ - solve 11054 (가장 긴 바이토닉 부분 수열)

---
+++ src/baekjoon/gold/gold_IV/Problem11054.java
@@ -0,0 +1,53 @@
+package baekjoon.gold.gold_IV;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem11054 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] lis = new int[n];
+        int[] lds = new int[n];
+
+        Arrays.fill(lis, 1);
+        Arrays.fill(lds, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (arr[j] < arr[i]) {
+                    lds[i] = Math.max(lds[i], lds[j] + 1);
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, lis[i] + lds[i] - 1);
+        }
+
+        bw.write(result + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
