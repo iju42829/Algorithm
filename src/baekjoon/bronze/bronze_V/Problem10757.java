8c82fcb32fd665338d551d1778404782e69f4fa7
iju42829 <ijud4282@gmail.com>
2025-06-06 21:08:38 +0900

    BOJ - solve 10757 (큰 수 A+B)

---
+++ src/baekjoon/bronze/bronze_V/Problem10757.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.bronze_V;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Problem10757 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        System.out.println(a.add(b));
+    }
+}
