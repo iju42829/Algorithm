e95d12e3e36ba16da027a25489189231e52a01ee
iju42829 <ijud4282@gmail.com>
2025-06-09 20:05:32 +0900

    BOJ - solve 10951 (A+B - 4)

---
+++ src/baekjoon/bronze/bronze_V/Problem10951.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem10951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
