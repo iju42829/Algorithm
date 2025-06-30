9d7d8fd7c9f39f8ac0dda1a1d4f0416173299c58
iju42829 <ijud4282@gmail.com>
2025-06-30 19:11:25 +0900

    BOJ - solve 11654 (아스키 코드)

---
+++ src/baekjoon/bronze/bronze_V/Problem11654.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println((int) (char) sc.next().charAt(0));
+    }
+}
