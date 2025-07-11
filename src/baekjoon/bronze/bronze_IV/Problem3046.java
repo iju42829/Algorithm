0ab5b888efd00cd1befbd0b62d7072e1d310f3b6
iju42829 <ijud4282@gmail.com>
2025-07-11 20:25:24 +0900

    BOJ - solve 3046 (R2)

---
+++ src/baekjoon/bronze/bronze_IV/Problem3046.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_IV;
+
+import java.util.Scanner;
+
+public class Problem3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println(2 * s - r1);
+    }
+}
