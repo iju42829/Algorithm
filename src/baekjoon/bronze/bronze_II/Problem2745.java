a715821a223f35dc1813de42558f48c2d63cd66f
iju42829 <ijud4282@gmail.com>
2025-07-23 19:49:14 +0900

    BOJ - solve 2745 (진법 변환)

---
+++ src/baekjoon/bronze/bronze_II/Problem2745.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.bronze_II;
+
+import java.util.Scanner;
+
+public class Problem2745 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+        int b = sc.nextInt();
+
+        System.out.println(Integer.parseInt(n, b));
+    }
+}
