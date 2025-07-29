e637c6b5bea0771702ad7e5781e26bf2da5aed0d
iju42829 <ijud4282@gmail.com>
2025-07-29 19:25:05 +0900

    BOJ - solve 31403 (A+B-C)

---
+++ src/baekjoon/bronze/bronze_IV/Problem31403.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.bronze_IV;
+
+import java.util.Scanner;
+
+public class Problem31403 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
+
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+
+    }
+}
