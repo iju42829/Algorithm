1ae5f0854c6caeb4500ca8e2f5bab50714f60ba9
iju42829 <ijud4282@gmail.com>
2025-08-06 19:33:33 +0900

    BOJ - solve 1550 (16진수)

---
+++ src/baekjoon/bronze/bronze_V/Problem1550.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String hex = sc.nextLine();
+        System.out.println(Integer.parseInt(hex, 16));
+    }
+}
