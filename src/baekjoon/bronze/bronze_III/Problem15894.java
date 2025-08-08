74653e0a4cb075312635b02742f382f22c378581
iju42829 <ijud4282@gmail.com>
2025-08-08 20:55:37 +0900

    BOJ - solve 15894 (수학은 체육과목 입니다)

---
+++ src/baekjoon/bronze/bronze_III/Problem15894.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_III;
+
+import java.util.Scanner;
+
+public class Problem15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLong() * 4);
+    }
+}
