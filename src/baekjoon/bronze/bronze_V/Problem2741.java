4d2a4820f02c7efbf32c8c26068a290c7acf830d
iju42829 <ijud4282@gmail.com>
2025-08-11 18:36:58 +0900

    BOJ - solve 2741 (N 찍기)

---
+++ src/baekjoon/bronze/bronze_V/Problem2741.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++)
+            System.out.println(i);
+    }
+}
