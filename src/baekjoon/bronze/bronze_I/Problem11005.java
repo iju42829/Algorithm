b9b6d0b2dfd51b6479ce0723e81107d8451f4056
iju42829 <ijud4282@gmail.com>
2025-08-13 00:06:49 +0900

    BOJ - solve 11005 (진법 변환 2)

---
+++ src/baekjoon/bronze/bronze_I/Problem11005.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_I;
+
+import java.util.Scanner;
+
+public class Problem11005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(Integer.toString(n, b).toUpperCase());
+    }
+}
