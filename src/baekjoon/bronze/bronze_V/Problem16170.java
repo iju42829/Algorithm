7765fee893e9424c6a3b872e95d3cab01f7e0f1f
iju42829 <ijud4282@gmail.com>
2025-08-16 21:54:14 +0900

    BOJ - solve 16170 (오늘의 날짜는?)

---
+++ src/baekjoon/bronze/bronze_V/Problem16170.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Problem16170 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
+        System.out.println(zonedDateTime.getYear());
+        System.out.println(zonedDateTime.getMonthValue());
+        System.out.println(zonedDateTime.getDayOfMonth());
+    }
+}
