9b295537c04618eeb8f0f432da388b2a229e603e
iju42829 <ijud4282@gmail.com>
2025-08-26 19:34:37 +0900

    BOJ - solve 2743 (단어 길이 재기)

---
+++ src/baekjoon/bronze/bronze_V/Problem2743.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next().length());
+    }
+}
