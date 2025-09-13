e5f6b1af429f69d55214d04e70ad1ba5d88ee282
iju42829 <ijud4282@gmail.com>
2025-09-13 23:36:31 +0900

    BOJ - solve 2440 (별 찍기 - 3)

---
+++ src/baekjoon/bronze/bronze_III/Problem2440.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.bronze_III;
+
+import java.util.Scanner;
+
+public class Problem2440 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
