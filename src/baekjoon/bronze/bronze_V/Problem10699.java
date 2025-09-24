1f241c911a6a3a03f07045250ead0c145eebc04c
iju42829 <ijud4282@gmail.com>
2025-09-24 18:53:42 +0900

    BOJ - solve 10699 (오늘 날짜)

---
+++ src/baekjoon/bronze/bronze_V/Problem10699.java
@@ -0,0 +1,9 @@
+package baekjoon.bronze.bronze_V;
+
+import java.time.*;
+
+public class Problem10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now(ZoneId.of("Asia/Seoul")));
+    }
+}
