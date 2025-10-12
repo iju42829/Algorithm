2bfd2734547bd793222a58c9fe8bb05d8bc6556c
iju42829 <ijud4282@gmail.com>
2025-10-12 20:41:01 +0900

    BOJ - solve 2914 (저작권)

---
+++ src/baekjoon/bronze/bronze_III/Problem2914.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_III;
+
+import java.util.Scanner;
+
+public class Problem2914 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), i = sc.nextInt();
+
+        System.out.println((i - 1) * a + 1);
+    }
+}
