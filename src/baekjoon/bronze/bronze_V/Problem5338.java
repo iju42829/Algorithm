44e4ddec99346be1431af17c750ae30fd8d5330a
iju42829 <ijud4282@gmail.com>
2025-11-04 21:28:35 +0900

    BOJ - solve 5338 (마이크로소프트 로고)

---
+++ src/baekjoon/bronze/bronze_V/Problem5338.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_V;
+
+public class Problem5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
