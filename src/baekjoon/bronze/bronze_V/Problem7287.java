9aaa2b8c2239ba4ca9c04819350683e4a54cea42
iju42829 <ijud4282@gmail.com>
2025-11-05 19:24:39 +0900

    BOJ - solve 7287 (등록)

---
+++ src/baekjoon/bronze/bronze_V/Problem7287.java
@@ -0,0 +1,8 @@
+package baekjoon.bronze.bronze_V;
+
+public class Problem7287 {
+    public static void main(String[] args) {
+        System.out.println("452");
+        System.out.println("iju42829");
+    }
+}
