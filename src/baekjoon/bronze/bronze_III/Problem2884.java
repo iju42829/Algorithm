03f3f6cd42ad6e305f917259c56281a83e1c14e3
iju42829 <ijud4282@gmail.com>
2025-11-08 21:16:34 +0900

    BOJ - solve 2884 (알람 시계)

---
+++ src/baekjoon/bronze/bronze_III/Problem2884.java
@@ -0,0 +1,22 @@
+package baekjoon.bronze.bronze_III;
+
+import java.util.Scanner;
+
+public class Problem2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt(), m = sc.nextInt();
+
+        m -= 45;
+
+        if (m < 0) {
+            m += 60;
+            h--;
+        }
+
+        if (h < 0) h = 23;
+
+        System.out.println(h + " " + m);
+    }
+}
