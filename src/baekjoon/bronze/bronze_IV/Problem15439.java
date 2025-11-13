ca6bdef5efb4bea16eb8bbbe1dcbd4d13492f033
iju42829 <ijud4282@gmail.com>
2025-11-13 15:45:36 +0900

    BOJ - solve 15439 (베라의 패션)

---
+++ src/baekjoon/bronze/bronze_IV/Problem15439.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze.bronze_IV;
+
+import java.util.Scanner;
+
+public class Problem15439 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(n * (n-1));
+    }
+}
