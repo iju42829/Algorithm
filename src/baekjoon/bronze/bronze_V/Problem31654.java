ceb755a87a749e503bf1b3413663db7a4f7e6919
iju42829 <ijud4282@gmail.com>
2025-11-18 16:27:57 +0900

    BOJ - solve 31654 (Adding Trouble)

---
+++ src/baekjoon/bronze/bronze_V/Problem31654.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem31654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+
+        System.out.println((a + b) == c ? "correct!" : "wrong!");
+    }
+}
