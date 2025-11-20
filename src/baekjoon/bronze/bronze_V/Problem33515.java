5a8ddb80b491907541e73aba78a83862c0f034ef
iju42829 <ijud4282@gmail.com>
2025-11-20 18:54:51 +0900

    BOJ - solve 33515 (노트북 세 대를 가지고 왔다)

---
+++ src/baekjoon/bronze/bronze_V/Problem33515.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem33515 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t1 = sc.nextInt(), t2 = sc.nextInt();
+
+        System.out.println(Math.min(t1, t2));
+    }
+}
