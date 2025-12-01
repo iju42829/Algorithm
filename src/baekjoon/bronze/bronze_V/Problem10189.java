e5485902fa00354d407ff2790f77a1623e74c814
iju42829 <ijud4282@gmail.com>
2025-12-01 19:43:52 +0900

    BOJ - solve 10189 (Hook)

---
+++ src/baekjoon/bronze/bronze_V/Problem10189.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze.bronze_V;
+
+public class Problem10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
