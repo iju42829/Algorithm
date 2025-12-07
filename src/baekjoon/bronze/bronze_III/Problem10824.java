23bea8d446b17c82d0bccc886da288fccb24758f
iju42829 <ijud4282@gmail.com>
2025-12-07 20:26:05 +0900

    BOJ - solve 10824 (네 수)

---
+++ src/baekjoon/bronze/bronze_III/Problem10824.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_III;
+
+import java.util.Scanner;
+
+public class Problem10824 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long aTemp = Long.parseLong(sc.next() + sc.next());
+        long bTemp = Long.parseLong(sc.next() + sc.next());
+
+        System.out.println(aTemp + bTemp);
+    }
+}
