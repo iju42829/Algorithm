e03b0fbb1e75057c3772245a4aac9cba1fd86129
iju42829 <ijud4282@gmail.com>
2025-12-20 18:55:40 +0900

    BOJ - solve 24078 (Remainder)

---
+++ src/baekjoon/bronze/bronze_V/Problem24078.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem24078 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+
+    System.out.println(x % 21);
+  }
+}
