8879a05d3d29a321e503def96a6ead797d8dde4e
iju42829 <ijud4282@gmail.com>
2025-12-31 19:51:33 +0900

    BOJ - solve 10821(정수의 개수)

---
+++ src/baekjoon/bronze/bronze_II/Problem10821.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_II;
+
+import java.util.Scanner;
+
+public class Problem10821 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println(sc.nextLine().split(",").length);
+  }
+}
