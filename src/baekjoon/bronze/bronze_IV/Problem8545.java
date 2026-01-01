5dc8524968a66bab88aaf8830112b4c2245a8f84
iju42829 <ijud4282@gmail.com>
2026-01-01 19:19:18 +0900

    BOJ - solve 8545

---
+++ src/baekjoon/bronze/bronze_IV/Problem8545.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_IV;
+
+import java.util.Scanner;
+
+public class Problem8545 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    StringBuilder sb = new StringBuilder(sc.nextLine());
+
+    System.out.println(sb.reverse().toString());
+  }
+}
