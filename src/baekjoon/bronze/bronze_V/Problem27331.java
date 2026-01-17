121f1abc76e99fee0c9ff85b2989d5b14a12e614
iju42829 <ijud4282@gmail.com>
2026-01-17 18:38:12 +0900

    BOJ - solve 27331 (Two-digit Integer)

---
+++ src/baekjoon/bronze/bronze_V/Problem27331.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem27331 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt(), b = sc.nextInt();
+
+    System.out.println(a * 10 + b);
+  }
+}
