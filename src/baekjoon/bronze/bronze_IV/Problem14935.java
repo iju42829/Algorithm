4f9d56c28cef2153305c94ba46e1553d2c979e0f
iju42829 <ijud4282@gmail.com>
2026-01-18 16:34:48 +0900

    BOJ - solve 14935 (FA)

---
+++ src/baekjoon/bronze/bronze_IV/Problem14935.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_IV;
+
+import java.util.Scanner;
+
+public class Problem14935 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    sc.nextLine();
+
+    System.out.println("FA");
+  }
+}
