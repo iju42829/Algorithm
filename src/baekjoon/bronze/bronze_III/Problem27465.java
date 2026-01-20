680b911ff970ad0f78b1672d4f3025e9a7ae5249
iju42829 <ijud4282@gmail.com>
2026-01-20 19:13:48 +0900

    BOJ - solve 27465 (소수가 아닌 수)

---
+++ src/baekjoon/bronze/bronze_III/Problem27465.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_III;
+
+import java.io.*;
+
+public class Problem27465 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    br.readLine();
+    System.out.println(1000000000);
+  }
+}
