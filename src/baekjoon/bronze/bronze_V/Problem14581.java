c4cecad5b7b87e853aa6e1d92c710964ae03224a
iju42829 <ijud4282@gmail.com>
2026-02-07 18:11:41 +0900

    BOJ - solve 14581 (팬들에게 둘러싸인 홍준)

---
+++ src/baekjoon/bronze/bronze_V/Problem14581.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem14581 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+
+    System.out.println(":fan::fan::fan:");
+    System.out.println(":fan::" + input + "::fan:");
+    System.out.println(":fan::fan::fan:");
+  }
+}
