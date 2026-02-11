0f5dd310265feba2a7935868aed68c2f4b11507e
iju42829 <ijud4282@gmail.com>
2026-02-11 19:57:14 +0900

    BOJ - solve 31606 (Fruit)

---
+++ src/baekjoon/bronze/bronze_V/Problem31606.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem31606 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt(), y = sc.nextInt();
+
+    System.out.println(x + y + 3);
+  }
+}
