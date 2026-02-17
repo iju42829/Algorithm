5eeced92f09b7fc1dab44eb6c5de3424bd06eea9
iju42829 <ijud4282@gmail.com>
2026-02-17 19:50:06 +0900

    BOJ - solve 33161 (Pencils 2)

---
+++ src/baekjoon/bronze/bronze_V/Problem33161.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem33161 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println(sc.nextInt() / 5);
+  }
+}
