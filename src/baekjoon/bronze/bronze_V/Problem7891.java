0fb970b09bdafa427a2468091d361a7417d18ba1
iju42829 <ijud4282@gmail.com>
2026-02-28 20:21:20 +0900

    BOJ - solve 7891 (Can you add this?)

---
+++ src/baekjoon/bronze/bronze_V/Problem7891.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem7891 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    for (int i = 0; i < t; i++) {
+      long x = sc.nextLong();
+      long y = sc.nextLong();
+
+      System.out.println(x + y);
+    }
+  }
+}
