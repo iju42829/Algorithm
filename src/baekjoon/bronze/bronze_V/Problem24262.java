d891eb681d3405e7acc132dd74b8fe3c06d2ba75
iju42829 <ijud4282@gmail.com>
2026-03-04 19:42:15 +0900

    BOJ - solve 24262 (알고리즘 수업 - 알고리즘의 수행 시간 1)

---
+++ src/baekjoon/bronze/bronze_V/Problem24262.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronze_V;
+
+import java.util.Scanner;
+
+public class Problem24262 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println(1);
+    System.out.println(0);
+  }
+}
